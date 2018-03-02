db696ce302b1567c276a8df88c915f8876b85f9a
Pablo Garcia <pablogarcia440@gmail.com>
2018-03-01 21:04:18 -0300

    Implantacao

---
+++ Aula2RMIComum/src/Escola.java
@@ -0,0 +1,9 @@
+import java.rmi.RemoteException;
+
+public class Escola extends Pessoa {
+	
+	public Escola() throws RemoteException{
+		super();		
+	}
+
+}
---
+++ Aula2RMIComum/src/Professor.java
@@ -0,0 +1,8 @@
+import java.rmi.RemoteException;
+
+public class Professor extends Pessoa{
+
+	public Professor() throws RemoteException{
+		super();		
+	}
+}
